338019136247a72459be2253a48a2fdafe936517
Olle Martensson <olle.martensson@digitalroute.com>
2012-10-05 17:24:27 +0200

    adding exec class

---
+++ src/com/digitalroute/lojic/LojicRealtimeExec.java
@@ -0,0 +1,31 @@
+package com.digitalroute.lojic;
+
+import com.digitalroute.devkit.drudr.DRUDR;
+import com.digitalroute.devkit.exception.DRException;
+import com.digitalroute.devkit.misc.DRClassUtils;
+import com.digitalroute.devkit.misc.DRLicenseCache;
+import com.digitalroute.devkit.storable.DRStorable;
+import com.digitalroute.devkit.wf.DRMIMEntry;
+import com.digitalroute.devkit.wf.DRRealtimeProcessor;
+
+
+public final class LojicRealtimeExec
+    extends DRRealtimeProcessor
+{
+    public void consume(DRUDR udr) throws DRException
+    {
+    }
+
+    public void consume(byte[] bytes) throws DRException
+    {
+    }
+
+    public void deinitialize() throws DRException
+    {
+    }
+
+    public void initialize(DRStorable config) throws DRException
+    {
+	
+    }
+}
